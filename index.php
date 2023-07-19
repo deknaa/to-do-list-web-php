74e7c17607c8e802be3e47998489366bae2838e7
Dekna <kdkrisnaaa@gmail.com>
2023-07-19 16:15:59 +0800

    Add gitignore, navbar, and home page section

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>To Do List</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    
+    <!-- Navbar -->
+    <nav class="navbar bg-primary">
+        <div class="container">
+            <a class="navbar-brand">
+                <img src="#" alt="Logo">
+                Logo
+            </a>
+                <form class="d-flex" role="search">
+                <button class="btn btn-outline-light" type="submit">Login</button>
+            </form>
+        </div>
+    </nav>
+    <!-- End Of Navbar -->
+
+    <!-- Hero Section -->
+    <main id='home'>
+        <div class="container">
+            <div class="row" id='batas_home'>
+                <div class="col-md-4">
+                    <img src="assets/Checklist.gif" alt="GIF" class='img-fluid'>
+                </div>
+                <div class="col-md-8 mt-5">
+                    <h1>To Do List</h1>
+                    <p>Buat kegiatan rencana harian anda dengan mudah dengan menggunakan website ini.</p>
+                    <a href="#" class="btn btn-primary shadow">Buat Rencana</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- End Of Hero Section -->
+
+    <!-- Bootstrap Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
