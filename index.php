d2690b3faeffaf079fec5533d65e35adf42b0640
Dekna <kdkrisnaaa@gmail.com>
2023-07-19 18:29:49 +0800

    added styling & service section at index.php

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
   <body>
     
     <!-- Navbar -->
-    <nav class="navbar bg-primary">
+    <nav class="navbar bg-nav">
         <div class="container">
-            <a class="navbar-brand">
+            <a class="navbar-brand d-flex">
                 <img src="#" alt="Logo">
-                Logo
+                <p class="text-primary-color">To Do List App</p>
             </a>
                 <form class="d-flex" role="search">
-                <button class="btn btn-outline-light" type="submit">Login</button>
+                <button class="btn btn-outline-light text-primary-color" type="submit">Login</button>
             </form>
         </div>
     </nav>
@@ -37,12 +37,51 @@
                 <div class="col-md-8 mt-5">
                     <h1>To Do List</h1>
                     <p>Buat kegiatan rencana harian anda dengan mudah dengan menggunakan website ini.</p>
-                    <a href="#" class="btn btn-primary shadow">Buat Rencana</a>
+                    <a href="#" class="btn btn-primary-color shadow">Buat Rencana</a>
                 </div>
             </div>
         </div>
     </main>
     <!-- End Of Hero Section -->
+
+    <!-- Layanan Section -->
+    <section id="layanan">
+        <div class="container">
+            <div class="wrapper_layanan">
+                <h2>Apa Yang Bisa Anda Lakukan?</h2>
+                <p>Berikut adalah beberapa hal yang bisa anda lakukan pada website ini</p>
+            </div>
+            <div class="card_layanan">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End of Layanan Section -->
 
     <!-- Bootstrap Script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
